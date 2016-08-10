0fabdbebcb2ca78fcdaf0f3fe76795ee992e2d04
Mohammed Shoukath <mohammed.shoukath@epsilon.com>
2016-08-10 13:28:26 -0400

    Refactored angular-cognalys to reuse success and error callbacks.

--- angular-cognalys.js
+++ angular-cognalys.js
@@ -1,66 +1,63 @@
-/*
+/**
  * Part of mcwebb/angular-cognalys
  * Modified to work with custom Python Flask API endpoint by: Omar Quazi
  * Copyright 2015 Matthew Webb <matthewowebb@gmail.com>
  * Copyright 2015 Omar Quazi <omarquaz@gmail.com>
  * MIT License
  */
-angular.module('py.cognalys', [])
-.provider('Cognalys', function () {
-	var apiEndpoint = 'http://localhost:5000';
+angular
+  .module('py.cognalys', [])
+  .provider('Cognalys', Cognalys);
 
-	this.$get = function ($http, $q) {
-		var cache = [];
-		return {
-			verifyMobileNumber: function (cc, number) {
-				cache = [];
-				var q = $q.defer();
-				$http.get(apiEndpoint+"/verify/placeCall/"+(cc+number))
-				.success(function (data) {
-					data = JSON.parse(data.result);
-					if (data.status == 'success') {
-						cache.push(data);
-						q.resolve(data);
-					} else {
-						if (typeof data.errors === 'object')
-							q.reject(data.errors);
-						else q.reject({
-							500: 'An unknown error occured'
-						});
-					}
-				})
+function Cognalys() {
+  var apiEndpoint = 'http://localhost:5000';
 
-				.error(function () {
-					q.reject({
-						500: 'An unknown error occured'
-					});
-				});
-				return q.promise;
-			},
-			confirmVerification: function (otp_end, pubKey, number) {
-				var q = $q.defer(),
-					latest = cache.pop();
-				$http.get(apiEndpoint+"/verify/verifyCall/"+latest.keymatch+"/"+(latest.otp_start+otp_end))
-				.success(function (data) {
-					data = JSON.parse(data.result);
-					if (data.status == 'success') {
-						cache.push(data);
-						q.resolve(data);
-					} else {
-						if (typeof data.errors === 'object')
-							q.reject(data.errors);
-						else q.reject({
-							500: 'An unknown error occured'
-						});
-					}
-				})
-				.error(function () {
-					q.reject({
-						500: 'An unknown error occured'
-					});
-				});
-				return q.promise;
-			}
-		};
-	};
-});
+  this.$get = function($http, $q) {
+    var cache = [],
+      deferred = $q.defer();
+
+    return {
+      verifyMobileNumber: verifyMobileNumber,
+      confirmVerification: confirmVerification
+    };
+
+    function verifyMobileNumber(cc, number) {
+      cache = [];
+      return makeCall(apiEndpoint + "/verify/placeCall/" + (cc + number));
+    }
+
+    function confirmVerification(otp_end, pubKey, number) {
+      var latest = cache.pop();
+      return makeCall(apiEndpoint + "/verify/verifyCall/" + latest.keymatch + "/" + (latest.otp_start + otp_end));
+    }
+
+    function makeCall(url) {
+      $http
+        .get(url)
+        .success(fnSuccess)
+        .error(fnError);
+      return deferred.promise;
+    }
+
+    function fnSuccess(data) {
+      var error = {};
+      data = JSON.parse(data.result);
+
+      if (data.status == 'success') {
+        cache.push(data);
+        deferred.resolve(data);
+      } else {
+        error = (typeof data.errors === 'object') ? data.errors : {
+          500: 'An unknown error occured'
+        };
+        deferred.reject(error);
+      }
+    }
+
+    function fnError(data) {
+      deferred.reject({
+        500: 'An unknown error occured'
+      });
+    }
+  };
+}
